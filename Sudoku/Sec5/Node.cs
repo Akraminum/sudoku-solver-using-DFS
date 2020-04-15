1c3052028d9c669d9fcdee2840f4543346e3d634
Akram <aminakram379@gmail.com>
2020-04-15 23:44:22 +0200

    proj

---
+++ Sudoku/Sec5/Node.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sec5
+{
+    class Node
+    {
+        public int[,] State { get; set; }
+        public int Depth { get; set; }
+        public int PathCost { get; set; }
+        public Node Parent { get; set; }
+
+        // root constructor
+        public Node(int[,] level)
+        {
+            this.State = level.Clone() as int[,];
+            this.Depth = 0;
+            this.PathCost = 0;
+            this.Parent = null;
+        }
+        // child constructor
+        public Node(Node current)
+        {
+            this.State = current.State.Clone() as int[,];
+            this.Depth = current.Depth + 1;
+            this.PathCost = current.PathCost + 1;
+            this.Parent = current;
+        }
+
+        // returns all possible childerns of current node with some conditions 
+        public List<Node> AddChildren()
+        {
+            List<Node> Childrens = new List<Node>();
+
+            for (byte row = 0; row < 9; row++)
+            {
+                for (byte col = 0; col < 9; col++)
+                {   
+                    // if this box is not empty
+                    if (this.State[row, col] ==  0)
+                    {
+                        for (byte label = 1; label < 10; label++)
+                        {
+                            // Check that this value has not already be used on this row &
+                            // Check that this value has not already be used on this column &
+                            // Check that this value has not already be used on this 3x3 square
+                            if (!(findInRow(row, label)) &
+                                !(findInCol(col, label)) &
+                                !(findInSquare(row, col, label)))
+                            {
+                                Childrens.Insert(0, new Node(this));
+                                Childrens[0].State[row, col] = label;
+
+                            }
+
+                        }
+                        return Childrens;
+                    }
+                }
+            }
+            return Childrens;
+        }
+
+
+        Boolean findInRow(int r, byte label)
+        {
+            for (byte c = 0; c < 9; c++)
+            {
+                if (this.State[r, c] == label)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        Boolean findInCol(int c, byte label)
+        {
+            for (byte r = 0; r < 9; r++)
+            {
+                if (this.State[r, c] == label)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        Boolean findInSquare(byte r, byte c, byte label)
+        {
+            byte delta_i = 0, delta_j = 0;
+
+            if (r < 3)
+            {
+                delta_i = 0; // 0 1 2
+            }
+            else if (r < 6)
+            {
+                delta_i = 1; // 3 4 5
+            }
+            else if (r < 9)
+            {
+                delta_i = 2; // 6 7 8
+            }
+            //~~~~~~~~~~~~~~~
+            if (c < 3)
+            {
+                delta_j = 0; // 0 1 2
+            }
+            else if (c < 6)
+            {
+                delta_j = 1; // 3 4 5
+            }
+            else if (c < 9)
+            {
+                delta_j = 2; // 6 7 8
+            }
+
+            for (byte i = 0; i < 3; i++)
+            {
+                for (byte j = 0; j < 3; j++)
+                {
+                    if (this.State[(3 * delta_i) + i, (3 * delta_j) + j] == label)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Sudoku/Sec5/Sudoku.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sec5
+{
+    public partial class Sudoku : Form
+    {
+        public Sudoku()
+        {
+            InitializeComponent();
+            init();
+
+
+        }
+
+        int[,] level = Levels.easy;
+        TextBox[,] textBoxes = new TextBox[9, 9];
+        string[] labels = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", };
+        List<int[,]> Solution = new List<int[,]>();
+        Node CurrentNode;
+
+        void init()
+        {
+            var allBoxes = Controls.OfType<TextBox>();
+            var boxes = allBoxes.ToArray();
+            //TextBox[,] textBoxes = new TextBox[9, 9]; 
+
+            for (byte i = 0; i < 9; i++)
+            {
+                for (byte j = 0; j < 9; j++)
+                {
+                    textBoxes[i, j] = boxes[((9 * i) + j)];
+                    textBoxes[i, j].TextChanged += new System.EventHandler(this.TextChange);
+                }
+            }
+            nodeToState(level);
+            richTextBox2.Text = "";
+            richTextBox1.Text = "";
+            CurrentNode = null;
+            List<int[,]> Solution = new List<int[,]>();
+        }
+
+        private void reset(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void TextChange(object sender, EventArgs e)
+        {
+            // our labels or empty
+            string res = Array.Find(labels, s => s.Equals((sender as TextBox).Text));
+            if (res == null)
+                (sender as TextBox).Text = "";
+
+        }
+  
+
+        private void Solveit(object sender, EventArgs e)
+        {
+            Boolean solved = SolveUsing_DFS();
+            if (solved)
+            {
+                richTextBox1.Text =
+                    "Depth:  " + Solution.Count() + " (Steps)" +"\n" +
+                    "Cost:    " + Solution.Count() * 1;
+                
+                foreach(var step in Solution)
+                {
+                    for(byte row = 0; row < 9; row++)
+                    {
+                        for (byte col = 0; col < 9; col++)
+                        {
+                            richTextBox2.Text += step[row,col] + "  ";
+                        }
+                        richTextBox2.Text += "\n";
+                    }
+                    richTextBox2.Text += "\n\n";
+                }
+
+                //MessageBox.Show("solved");
+            }
+            else
+            {
+                MessageBox.Show("can't");
+            }
+        }
+       
+        //========================== depth first algorithm ============================
+        private Boolean SolveUsing_DFS()
+        {
+            Stack<Node> DFS_Stack = new Stack<Node>();
+            Node InitialState = new Node(level);
+            DFS_Stack.Push(InitialState);
+
+            while (DFS_Stack.Count != 0)
+            {
+                //remove
+                CurrentNode = DFS_Stack.Pop();
+                
+                //check goal
+                if ( isComplete(CurrentNode.State))
+                {
+                    //write it to screen
+                    nodeToState(CurrentNode.State);
+                    // return solution
+                    while (CurrentNode != null)
+                    {
+                        Solution.Insert(0, CurrentNode.State);
+                        CurrentNode = CurrentNode.Parent;
+                    }
+                    
+                    return true;
+                }
+                //add children
+                if (CurrentNode.Depth == 100)
+                {
+                    continue;
+                }
+                foreach (var child in CurrentNode.AddChildren())//<5,1>
+                {
+                    DFS_Stack.Push(child);
+                }
+
+            }
+
+            return false;
+        }
+        //======================================================
+
+
+        void nodeToState(int[,] state)
+        {
+            for (byte row = 0; row < 9; row++)
+            {
+                for (byte col = 0; col < 9; col++)
+                {
+                    textBoxes[8-row , 8-col].Text = state[row, col].ToString(); // state[row, col].ToString();
+                }
+            } 
+        }
+        Boolean isComplete(int[,] state)
+        {
+            for (byte row = 0; row < 9; row++)
+            {
+                for (byte col = 0; col < 9; col++)
+                {
+                    if (state[row, col] == 0)
+                        return false;
+                }
+            }
+            // if it is complete
+            return true;
+        }
+    }
+}
